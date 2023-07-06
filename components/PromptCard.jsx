88b9a48b023a9434ea5667a8200477617fbefe51
Suchanapal <122770752+Suchanapal@users.noreply.github.com>
2023-07-06 12:24:49 +0600

    fix typo

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -22,30 +22,37 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   };
 
   const handleProfileClick = () => {
-    console.log(post);
+    
 
-    if (post.creator._id === session?.user.id) return router.push("/profile");
+    if (post.creator?._id === session?.user.id) return router.push("/profile");
 
-    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+    router.push(`/profile/${post.creator?._id}?name=${post.creator?.username}`);
   };
 
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-center mb-2">
-        <div className="flex justify-start items-center gap-3 cursor-pointer"onClick={handleProfileClick}>
-          <Image
-            src={post.creator.image}
-            alt="user_image"
-            width={40}
-            height={40}
-            className="rounded-full object-contain"
-          />
+        <div
+          className="flex justify-start items-center gap-3 cursor-pointer"
+          onClick={handleProfileClick}
+        >
+          {post.creator?.image ? (
+            <Image
+              src={post.creator.image}
+              alt="user_image"
+              width={40}
+              height={40}
+              className="rounded-full object-contain"
+            />
+          ) : (
+            <div className="rounded-full bg-gray-200 w-10 h-10"></div>
+          )}
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">
-              {post.creator.username}
+              {post.creator?.username || "Unknown User"}
             </h3>
             <p className="font-inter text-sm text-gray-500">
-              {post.creator.email}
+              {post.creator?.email || "Unknown Email"}
             </p>
           </div>
         </div>
@@ -80,7 +87,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         {post.tag}
       </p>
 
-      {session?.user.id === post.creator._id && pathName === "/profile" && (
+      {post.creator && session?.user?.id === post.creator._id && pathName === "/profile" && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p
             className="font-inter text-sm text-teal-500 cursor-pointer "
